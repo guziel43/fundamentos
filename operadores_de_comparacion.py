5a61dc0c0f32b9fea2538a7d649a876ec826c2c0
guziel43 <guziel43@gmail.com>
2022-06-03 14:57:30 -0500

    03 de junio 2022

---
+++ operadores_de_comparacion.py
@@ -0,0 +1,11 @@
+
+operandoA=int(input("Anota un numero entero: "))
+operandoB=int(input("Anota otro numero entero: "))
+
+print(f"{operandoA} es igual a {operandoB}: ",operandoA==operandoB)
+print(f"{operandoA} es igual a {operandoB}: ", operandoA!=operandoB)
+print(f"{operandoA}  es mayor a {operandoB}: ", operandoA>operandoB)
+print(f"{operandoA}  es menor a {operandoB}: ", operandoA<operandoB)
+print(f"{operandoA}  es mayor a {operandoB}: ", operandoA>=operandoB)
+print(f"{operandoA}  es menor a {operandoB}: ", operandoA<=operandoB)
+
